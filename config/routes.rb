85540380e6ccd8e4fe3808014c5d67dd23673f13
Afke Holgersen <Afke@macs-MacBook-Pro.local>
2016-10-09 23:46:18 -0700

    added routes and links to webpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'main/home'
+
   resources :trainers
   resources :tokimons
-  
+
+  root to: 'main#home' #main home webpage
+  get "tokimons/index"
+  get "trainers/index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
